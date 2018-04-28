00ce8b325739c5026747da6a22d3d5233e047170
Simon Bao <Simonbao.sb@gmail.com>
2018-04-28 17:56:46 +0100

    Passing Test - CardGame
    CardGame instance construct initializes deck with 52 cards

--- src/CardGame.php
+++ src/CardGame.php
@@ -1,10 +1,17 @@
 <?php
-class CardGame{
-  public static function sayHello()
-  {
-    return 'Hello';
+
+  class CardGame{
+    private $deck;
+
+    function __construct(){
+      $this->deck = range(1,52);
+    }
+
+    public function getDeck()
+    {
+      return $this->deck;
+    }
   }
-}
 
 
 
