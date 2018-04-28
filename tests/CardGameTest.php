744dc71546fbf86cd4ef843dbc213a7cb7b66fe2
Simon Bao <Simonbao.sb@gmail.com>
2018-04-28 18:31:43 +0100

    Failing Test - CardGame
    Dealed hand should have 7 cards

--- tests/CardGameTest.php
+++ tests/CardGameTest.php
@@ -7,7 +7,14 @@ class CardGameTest extends TestCase{
   public function testDeckCardCount()
   {
     $card_game = new CardGame();
-    $deck = $card_game->getDeck();
-    $this->assertEquals(52, sizeof($deck));
+    $deck_length = sizeof($card_game->getDeck());
+    $this->assertEquals(52, $deck_length);
   } 
+
+  public function testDealedSevenCards()
+  {
+    $card_game = new CardGame();
+    $dealed_hand_length = sizeof($card_game->dealCards());
+    $this->assertEquals(7, $dealed_hand_length);
+  }
 }
