c88134e6ab3e7787cd9c12ebaf347eab86a0a204
Simon Bao <Simonbao.sb@gmail.com>
2018-04-29 16:20:22 +0100

    Failing Test - CardGame
    Refactored CardGameTest to remove Deck dependency through dependency injection and mocks
    
    Fixed consistency in structure

--- tests/CardGameTest.php
+++ tests/CardGameTest.php
@@ -3,13 +3,16 @@
 use PHPUnit\Framework\TestCase;
 
 class CardGameTest extends TestCase{
+
   # ------------------ HELPERS ------------------
 
   protected $card_game;
   protected $deck_length;
 
   protected function setUp(){
-    $this->card_game = new CardGame();
+    $deckStub = $this->createMock(Deck::class);
+    $deckStub->method('cards')->willReturn(range(1,52));
+    $this->card_game = new CardGame($deckStub);
     $this->deck_length = sizeof($this->card_game->getDeck());
   }
 
@@ -25,26 +28,22 @@ class CardGameTest extends TestCase{
 
   # ------------------ TESTS ------------------
 
-  public function testDeckCardCount()
-  {
+  public function testDeckCardCount(){
     $this->assertEquals(52, $this->deck_length);
   } 
 
-  public function testDealedSevenCards()
-  {
+  public function testDealedSevenCards(){
     $dealed_hand_length = sizeof($this->dealCards());
     $this->assertEquals(7, $dealed_hand_length);
   }
 
-  public function testDealedCardsRemovesSevenFromDeck()
-  {
+  public function testDealedCardsRemovesSevenFromDeck(){
     $this->assertEquals(52, $this->deck_length);
     $this->dealCards();
     $this->assertEquals(45, $this->updateDeck());
   }
 
-  public function testDealedCardsRemovedFromDeck()
-  {
+  public function testDealedCardsRemovedFromDeck(){
     $cards = $this->dealCards();
     foreach($cards as $card){
       $this->assertNotContains($card, $this->card_game->getDeck());
