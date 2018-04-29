8611de65f11103bd6b47acba29d9e5cedadecf35
Simon Bao <Simonbao.sb@gmail.com>
2018-04-29 17:42:04 +0100

    Passing Test - Deck
    First card is 'Ace of Hearts'

--- src/Deck.php
+++ src/Deck.php
@@ -1,13 +1,33 @@
 <?php
   class Deck{
-    private $cards;
+    private $cards = [];
+    const SUITS = array(
+      'Hearts', 'Clubs', 'Spades',
+      'Diamonds');
+
+    const CARD_SET = array(
+      'Ace', 'Two', 'Three',
+      'Four', 'Five', 'Six',
+      'Seven', 'Eight', 'Nine',
+      'Ten', 'Jack', 'Queen',
+      'King'
+    );
 
     function __construct(){
-      $this->cards = range(1,52);
+      $this->buildDeck();
     }
 
     public function cards(){
       return $this->cards;
     }
 
+    private function buildDeck(){
+      $suits = Deck::SUITS;
+      $set = Deck::CARD_SET;
+      foreach($suits as $suit){
+        foreach($set as $card){
+          array_push($this->cards, array($suit, $card));
+        }
+      }
+    }
   }
