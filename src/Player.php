474deb8c0c626883af2ce9ade0efd0d1bedca6c4
Simon Bao <Simonbao.sb@gmail.com>
2018-04-30 18:03:56 +0100

    Refactor Player/PlayerTest
    Removed unused constructor
    
    Renamed function from hand to getHand to follow consistency
    
    Additional test helper methods created to ensure single responsibility and improve maintainability

--- src/Player.php
+++ src/Player.php
@@ -3,14 +3,13 @@
 class Player{
   private $hand = [];
 
-  public function __construct(){
-  }
-
-  public function hand(){
+  public function getHand(){
     return $this->hand;
   }
+  // The getHand function provides encapsulation and returns instance variable hand.
 
   public function obtainCard($card){
     array_push($this->hand, $card); 
   }
+  // The obtainCard method recieves a single argument(card) and adds it to the instance variable hand.
 }
--- tests/PlayerTest.php
+++ tests/PlayerTest.php
@@ -11,29 +11,44 @@ class PlayerTest extends TestCase{
     $this->player = new Player();
   }
 
-  protected function givePlayerCard($player, $suit, $rank){
-    $player->obtainCard(array($suit, $rank));
+  private function givePlayerCard($player, $card){
+    $player->obtainCard($card);
   }
 
+  private function createCard($suit, $rank){
+    $created_card = array($suit, $rank);
+    return $created_card;
+  }
+
+  private function getHand(){
+    $hand = $this->player->getHand();
+    return $hand;
+  }
   # ------------------ TESTS ------------------
 
   public function testEmptyHand(){
-    $player_hand = $this->player->hand();
+    $player_hand = $this->getHand();
     $this->assertEmpty($player_hand);
   }
+  // This tests each player instance starts with an empty hand.
 
   public function testPlayerRecievesCard(){
     $player = $this->player;
-    $this->givePlayerCard($player, 'Hearts', 'Ace');
-    $this->assertArraySubset([['Hearts', 'Ace']], $player->hand());
+    $card = $this->createCard('Hearts', 'Ace');
+    $this->givePlayerCard($player, $card);
+    $this->assertArraySubset([['Hearts', 'Ace']], $this->getHand());
   }
+  // This tests if the argument passed to player is stored as intended.
 
   public function testPlayerRecievesMultipleCards(){
     $player = $this->player;
-    $this->givePlayerCard($player, 'Spades', 'King');
-    $this->assertEquals([['Spades', 'King']], $player->hand());
-    $this->givePlayerCard($player, 'Clubs', 'Three');
-    $this->assertEquals([['Spades', 'King'],['Clubs', 'Three']], $player->hand());
+    $king_of_spades = $this->createCard('Spades', 'King');
+    $this->givePlayerCard($player, $king_of_spades);
+    $this->assertEquals([$king_of_spades], $this->getHand());
+    $three_of_clubs = $this->createCard('Clubs', 'Three');
+    $this->givePlayerCard($player, $three_of_clubs);
+    $this->assertEquals([$king_of_spades, $three_of_clubs], $this->getHand());
   }
+  // This tests if the multiple argument passed to player are stored correctly.
 
 }
