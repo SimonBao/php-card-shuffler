b390f231672767c7d1de9eae365da8d57c3578be
Simon Bao <Simonbao.sb@gmail.com>
2018-05-02 02:12:25 +0100

    Failing Test - DeckTest
    Checking for perfectly shuffled deck with no cards in sequence.

--- tests/DeckTest.php
+++ tests/DeckTest.php
@@ -24,6 +24,29 @@ class DeckTest extends TestCase{
   /* This is a PHPUnit fixture which runs before each test. This enables creating clean testing environment,
   and enables setting up code which is required by every test.
   */
+
+  private function checkCardSquence($cards, $i){
+    $next_iteration = $i + 1;
+    $current_card = $this->createCard($cards[$i]);
+    $next_card = $this->createCard($cards[$next_iteration]);
+    if($current_card[0] == $next_card[0]){
+      $this->assertNotEquals($current_card[1]+1, $next_card[1]);
+    }
+  }
+  /* 
+  Helper method creates current card and next card via another helper method,
+  if both card suits are equal then expects next card to not match sequence.
+  */
+
+  private function createCard($card){
+    $card_suit = $this->getCardSuit($card);
+    $card_numeral_rank = $this->getCardRank($card);
+    return [$card_suit, $card_numeral_rank];
+  }
+  /* 
+  The createdCard method rebuilds the card argument and returns it with,
+  value represented by integer value instead of string.
+  */
   
   private function createCardSuit($suit, $ranks){
     $suit_ranks = [];
@@ -69,6 +92,21 @@ class DeckTest extends TestCase{
     $removed_card = $this->deck->removeCard();
     return $removed_card;
   }
+
+  private function getCardSuit($card){
+    $card_suit = $card[0];
+    return $card_suit;
+  }
+  /* Returns the card suit */
+
+  private function getCardRank($card){
+    $card_rank = array_search($card[1], Deck::CARD_RANKS);
+    return $card_rank;
+  }
+  /* 
+  Returns the card rank from String type to relative integer value,
+  based on position in ranking system.
+  */
 
   # ------------------ TESTS ------------------
 
@@ -143,5 +181,22 @@ class DeckTest extends TestCase{
     $unshuffled_cards = $unshuffled_deck->getCards();
     $this->assertNotEquals($shuffled_cards, $unshuffled_cards);
   }
+  /* 
+  The testShufflecards method expects cards to be shuffled, it does this by
+  comparing a shuffled deck with a new sequenced
+  */
+
+  public function testShuffledSequence(){
+    $deck = $this->deck;
+    $deck->shuffleCards();
+    $cards = $deck->getCards();
+    for($i = 0; $i < sizeof($cards)-1 ; $i++){
+      $card = $cards[$i];
+      $this->checkCardSquence($cards, $i);
+    }
+  }
+  /* 
+  Expects none of the cards are in sequence, tests card for suit and next value.
+  */
 
 }
